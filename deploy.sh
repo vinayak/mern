bea53d855992611db89259880745a97b8b60faba
Vinayak <vinayak.malavade@searshc.com>
2018-01-19 16:16:03 +0530

    update

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Deploying React app to the server....."
+npm run build
+tar -czf build.tar.gz build/
+tar -czf server.tar.gz server/ --exclude node_modules
+scp build.tar.gz user@35.229.40.87:/usr/share/nginx/.
+scp server.tar.gz user@35.229.40.87:/usr/share/nginx/.
+rm build.tar.gz server.tar.gz
+
+ssh user@35.229.40.87 << ENDSSH
+cd /usr/share/nginx
+rm -rf build
+tar -xzf build.tar.gz
+rm build.tar.gz
+pm2 stop sears
+rm -rf server
+tar -xzf server.tar.gz
+rm server.tar.gz
+cd server
+npm install
+pm2 start sears
+ENDSSH
--- src/pages/users/login.js
+++ src/pages/users/login.js
@@ -21,8 +21,8 @@ class Login extends Component {
         // console.log(res.data)
         // console.log(self.props);
         console.log(res.data.token);
-        self.props.action.login(res.data.token)
-        window.location=window.location.protocol + '//vinay.'+ window.location.hostname +":"+ window.location.port
+        window.location=window.location.protocol + '//vinay.'+ window.location.hostname +":"+ window.location.port+'/list'
+        self.props.action.login(res.data.token) //not storing in subdomain
       })
       .catch(function(err){
         console.log(err.response);
